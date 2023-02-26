1349bf8b8498eb33496d40db11f9a459068fb5a2
salvador-villanueva <71253158+salvador-villanueva@users.noreply.github.com>
2023-02-26 15:19:01 -0800

    Submission page working

--- src/Submitted.js
+++ src/Submitted.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Submitted() {
   return (
-    <div>Submitted</div>
+    <div>
+        <h1 style={{textAlign: "center"}}>Thank you for your submission!</h1>
+        <h3 style={{textAlign: "center"}}>
+            Your feedback is anonymous and will be sent to your professor for attendance.
+        </h3>
+        <br></br>
+        <div style={{
+                width:"100%", display:"flex", 
+                flexDirection:"column", 
+                justifyContent:"center",
+                alignItems:"center"}}>
+        <Link to="/"><button>Home </button></Link>
+        </div>
+
+    </div>
+    
+    
+    
   )
 }
 
