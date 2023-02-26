70e9bdb1a7c9b1d3faff0671180021f115ff22d1
salvador-villanueva <71253158+salvador-villanueva@users.noreply.github.com>
2023-02-26 15:03:31 -0800

    All the routing and imports

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import Landing from "./Landing";
 import SelectClasses from "./selectClasses";
 import Survey from "./Survey";
+import {Route, Routes } from "react-router-dom";
+import Submitted from "./Submitted";
+
 
 
 
 function App() {
   return (
-    <>
-      <Landing />
-      <SelectClasses />
-      <Survey />
-    </>
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route path="/Schedule" element={<SelectClasses/>}/>
+      <Route path="/Survey" element={<Survey/>}/>
+      <Route path="/Submitted" element={<Submitted/>}/>
+    </Routes>
   );
 }
 
