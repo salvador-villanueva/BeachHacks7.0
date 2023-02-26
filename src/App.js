d6187e5344e94182f69d44ee430151bb79268bc9
salvador-villanueva <71253158+salvador-villanueva@users.noreply.github.com>
2023-02-26 11:15:26 -0800

    Deleted all the unessecary stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Start your survey</h1>
-    </div>
+    <>
+      {/* <Landing.js> */}
+
+    </>
   );
 }
 
