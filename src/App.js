fea66cd1ee3348487d36da793a91aae0ad0bb993
Jose Fuentes <61329069+fuentesjm@users.noreply.github.com>
2023-02-26 12:17:30 -0800

    um

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
+import React from 'react'
+import SelectClasses from "./selectClasses";
 
 
 
 function App() {
   return (
     <>
-      <Landing />
-      <input type="text" />
+      <SelectClasses />
     </>
   );
 }
---
+++ src/selectClasses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function SelectClasses() {
+    return (
+        <div>
+            <button>CS113: Programming in Java</button>
+            <button>CECS323: Fundamental of Databases</button>
+            <button>CECS328: Algorithms </button>
+            <button>CS329: Theory</button>
+        </div>
+    )
+}
