15e37399c4ee7f48aa1f8c67f0d34db915e0efc1
Jose Fuentes <61329069+fuentesjm@users.noreply.github.com>
2023-02-26 12:21:21 -0800

    Update selectClasses.js

--- src/selectClasses.js
+++ src/selectClasses.js
@@ -3,10 +3,17 @@ import React from 'react';
 export default function SelectClasses() {
     return (
         <div>
-            <button>CS113: Programming in Java</button>
-            <button>CECS323: Fundamental of Databases</button>
-            <button>CECS328: Algorithms </button>
-            <button>CS329: Theory</button>
+            <h1 style={{textAlign: "center"}}> Select a class<br></br> to give feedback</h1>
+            <div style={{
+                width:"100%", display:"flex", 
+                flexDirection:"column", 
+                justifyContent:"center",
+                alignItems:"center"}}>
+                <button style={{textAlign: "center"}}>CS113: Programming in Java</button>
+                <button>CECS323: Fundamental of Databases</button>
+                <button>CECS328: Algorithms </button>
+                <button>CS329: Theory</button>
+            </div>
         </div>
     )
 }
