05686123e446c1d6de7afc609a5573aa55edaa13
salvador-villanueva <71253158+salvador-villanueva@users.noreply.github.com>
2023-02-26 12:23:37 -0800

    landing page centered

--- src/Landing.js
+++ src/Landing.js
@@ -3,7 +3,17 @@ import React from 'react'
 export default function Landing() {
   return (
     <div>
-        Landing
+        <h1 style={{textAlign: "center"}}> Welcome to <br></br> RateMyLecture</h1>
+        <div style={{
+            width:"100%", display:"flex", 
+            flexDirection:"column", 
+            justifyContent:"center",
+            alignItems:"center"}}>
+            <button style={{textAlign: "center"}}>
+                SSO</button>
+            <button>Gmail</button>
+            <button>AppleID</button>
+        </div>
     </div>
   )
 }
